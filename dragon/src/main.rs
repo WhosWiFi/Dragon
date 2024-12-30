b84767e5712064b852ccf992e25b279cf38ebc07
WhosWiFi <WhosWiFi@gmail.com>
2024-12-30 18:23:36 -0500

    can run pwd and add /

--- dragon/src/main.rs
+++ dragon/src/main.rs
@@ -7,10 +7,13 @@ use regex::Regex;
 fn read_file(flag:&String, file_path:&String) {
     println!("The Flag Provided: {:?}", flag);
     println!("The File Path Provided: {:?}", file_path);
-    let command_output = Command::new("pwd").output().expect("Could not execute command properly");
+    let mut command_output = Command::new("pwd").output().expect("Could not execute command properly");
     println!("The Status Code is: {}", command_output.status);
     println!("The Output of the command pwd: {}", String::from_utf8_lossy(&command_output.stdout));
     println!("The Standard Error is: {}", String::from_utf8_lossy(&command_output.stderr));
+    let current_directory = String::from_utf8_lossy(&command_output.stdout).trim().to_string();
+    let directory_create_file = current_directory + "/";
+    println!("This is the file path:\n {}", &directory_create_file);
 }
 
 fn main() {
