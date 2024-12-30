a6c4b43b1526e37bf7a971e8749949d00931dde3
WhosWiFi <WhosWiFi@gmail.com>
2024-12-30 15:37:10 -0500

    can read text file from computer

--- dragon/src/main.rs
+++ dragon/src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+use std::fs;
 fn main() {
     println!("The Dragon is Born 🐉");
+    println!("Select the file you would like to Compress (-c) or Decompress (-d)");
+    let file = fs::read_to_string("C:/Users/Carson/Desktop/rust.txt").expect("Could not open file :(\n");
+    println!("The contents of the file:\n {file}");
 }
