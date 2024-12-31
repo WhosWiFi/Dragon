20732d1d1223176d4b95cc4d9995885a0d2d08c3
WhosWiFi <WhosWiFi@gmail.com>
2024-12-30 20:06:36 -0500

    added gui with eframe and egui, buttons for Compress and Decompress

--- dragon/src/main.rs
+++ dragon/src/main.rs
@@ -1,11 +1,11 @@
 use std::env;
 use std::fs;
 use std::process::Command;
-use std::process::Output;
 use regex::Regex;
+use eframe::egui;
 
 fn read_file(flag:&String, file_path:&String) {
-    let mut command_output = Command::new("pwd").output().expect("Could not execute command properly");
+    let command_output = Command::new("pwd").output().expect("Could not execute command properly");
     let current_directory = String::from_utf8_lossy(&command_output.stdout).trim().to_string();
     let directory_create_file = current_directory + "/";
     println!("This is the file path:\n {}", &directory_create_file);
@@ -22,10 +22,42 @@ fn read_file(flag:&String, file_path:&String) {
 
 fn main() {
     println!("The Dragon is Born 🐉");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("Dragon", native_options, Box::new(|cc| Ok(Box::new(Dragon::new(cc)))));
     println!("How to use Dragon -> dragon [flags: -c (compress), -d (decompress)] 'path_to_file']");
     let cli_args: Vec<String> = env::args().collect();
     println!("{:?}", cli_args);
     let flag: &String = &cli_args[1];
     let file_path: &String = &cli_args[2];
     read_file(flag, file_path);
+}
+
+#[derive(Default)]
+struct Dragon {}
+
+impl Dragon {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+
+    fn ui_counter(&mut self, ui: &mut egui::Ui) {
+        ui.horizontal(|ui| {
+            if ui.button("Compress File").clicked() {
+                println!("Compress File Button Clicked");
+            }
+            ui.label("TEST");
+            if ui.button("Decompress File").clicked() {
+                println!("Decompress File Button Clicked");
+            }
+        });
+    }
+}
+
+impl eframe::App for Dragon {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Welcome to Dragon");
+            self.ui_counter(ui);
+        });
+    }
 }
